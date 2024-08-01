5d36c49c2c252d74cb627d9f2dcb2f64965f5de4
Raymundo Jimenez <raymundo.munoz@3pillarglobal.com>
2024-07-31 18:08:14 -0600

    sct-04 Adding missing components

---
+++ src/app/heroes/components/hero/hero.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+  public name: string = 'ironman';
+  public age: number = 45;
+
+  public changeHeroName(name: string): void {
+    this.name = name;
+  }
+
+  public changeAge(age: number): void {
+    this.age = age;
+  }
+
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${ this.name } - ${ this.age }`;
+  }
+
+  resetForm():void {
+    this.name = 'ironman';
+    this.age = 45;
+  }
+}
